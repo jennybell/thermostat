5d91e8685199dbf130dd9176331035b12a88f876
Jenny <jennybell42@gmail.com>
2021-06-06 09:41:09 +0100

    fixed update temperature constant

--- interface.js
+++ interface.js
@@ -1,36 +1,36 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-  // const updateTemperature() = () => {
-  //   document.querySelector('#temperature').innerText = thermostat.temperature;
-  // }
+   const updateTemperature = () => {
+     document.querySelector('#temperature').innerText = thermostat.temperature;
+   }
 
   const thermostat = new Thermostat();
-  document.querySelector('#temperature').innerText = thermostat.temperature;
+  updateTemperature();
 
   document.querySelector('#temp_up').addEventListener('click', () => {
     thermostat.upTemperature();
-    document.querySelector('#temperature').innerText = thermostat.temperature;
+    updateTemperature()
   })  
 
   document.querySelector('#temp_down').addEventListener('click', () => {
     thermostat.downTemperature();
-    document.querySelector('#temperature').innerText = thermostat.temperature;
+    updateTemperature();
   }) 
 
   document.querySelector('#temp_reset').addEventListener('click', () => {
     thermostat.reset();
-    document.querySelector('#temperature').innerText = thermostat.temperature;
+    updateTemperature();
   })
 
   document.querySelector('#PSM_on').addEventListener('click', () => {
     thermostat.switchPowerSavingModeOn();
-    document.querySelector('#power-saving-status').innerText = 'on';
-    document.querySelector('#temperature').innerText = thermostat.temperature;
+    document.querySelector('#power-saving-status').innerText = 'ON';
+    updateTemperature();
   })
 
   document.querySelector('#PSM_off').addEventListener('click', () => {
     thermostat.switchPowerSavingModeOff();
-    document.querySelector('#power-saving-status').innerText = 'off';
-    document.querySelector('#temperature').innerText = thermostat.temperature;
+    document.querySelector('#power-saving-status').innerText = 'OFF';
+    updateTemperature();
   })
 })
